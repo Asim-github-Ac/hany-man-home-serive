d7bc4414c2374145b677076adfe5b237eb45590e
Asim Mehar <asimmeharproffessional@gmail.com>
2022-06-18 21:19:15 +0100

    home services

---
+++ app/src/main/java/com/kvsn/builds/cap1/AboutUs.java
@@ -0,0 +1,17 @@
+package com.kvsn.builds.cap1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUs extends AppCompatActivity
+{
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState)
+     {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_about_us);
+	    setTitle("About Us");
+     }
+}
